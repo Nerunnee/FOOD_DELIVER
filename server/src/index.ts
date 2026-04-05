8561bc6038ae9cc88c6ed9b9b826e0bb992c7e89
Nerunnee <e.norvoo@gmail.com>
2026-04-05 19:13:36 +0800

    s

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,11 +9,11 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
 app.use(
   cors({
     origin: "https://food-delivery-wy23.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
