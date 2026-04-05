5dfcdcad6557fbba232923c5ba4bb5b587fda861
Nerunnee <e.norvoo@gmail.com>
2026-04-05 18:58:13 +0800

    cor methods

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://food-delivery-wy23.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.use("/users", usersRouter);
 app.use("/categories", categoriesRouter);
